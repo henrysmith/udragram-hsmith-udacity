a2dcfee059fb20f8221510a5c8d30486915fafe3
Smith <hyso@gft.com>
2021-02-21 01:30:20 -0500

    Adding library http-status-codes to avoid using magic number. Adding function to to filter image as required in the exercise Adding method to get and validate a parameter from the query string Adding  try-catch and reject to avoid UnhandledPromiseRejection exception to hang the server in a promises chaining.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import {  
+  StatusCodes,
+  ReasonPhrases
+} from 'http-status-codes';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles, getParameter } from './util/util';
 
 (async () => {
 
@@ -9,7 +13,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -17,30 +21,41 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
+  // QUERY PARAMETERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-
-  //! END @TODO1
   
+  //! END @TODO1
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
+
+  app.get("/filteredimage", async (req, res, next) => {
+    return getParameter(req.query, "image_url")
+      .then(image_url => filterImageFromURL(image_url))
+      .then(fileURLToPath =>
+        res.status(StatusCodes.CREATED).sendFile(fileURLToPath, { "Content-Type": "image/jpeg" }, () => deleteLocalFiles([fileURLToPath])))
+      .catch(error => {
+        console.error(error);
+        res.status(StatusCodes.UNPROCESSABLE_ENTITY).send(!!error.message ? ReasonPhrases.UNPROCESSABLE_ENTITY : error)
+      });
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,6 +1,13 @@
+import { Request, Response } from 'express';
 import fs from 'fs';
 import Jimp = require('jimp');
-
+import { NextFunction } from 'connect';
+import {
+    ReasonPhrases,
+    StatusCodes,
+    getReasonPhrase,
+    getStatusCode,
+} from 'http-status-codes';
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
 // returns the absolute path to the local image
@@ -8,18 +15,22 @@ import Jimp = require('jimp');
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
-    });
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const photo = await Jimp.read(inputURL);
+            const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+            await photo
+                .resize(256, 256) // resize
+                .quality(60) // set JPEG quality
+                .greyscale() // set greyscale
+                .write(__dirname + outpath, (img) => {
+                    resolve(__dirname + outpath);
+                });
+        } catch (error) {                        
+            reject(error);            
+        }
+    } );
 }
 
 // deleteLocalFiles
@@ -27,8 +38,52 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
+export async function deleteLocalFiles(files: Array<string>) {
+    for (let file of files) {
         fs.unlinkSync(file);
     }
+}
+
+// getParameter
+// helper function to get a parameter from query string object
+// if the parameter is not present then returns an exception.
+// INPUTS
+//    query: query parameter object.
+//    parameterName: parameter expected.
+//  RETURN
+//    given parameter value in query string object.
+//  EXCEPTIONS
+//    Error on parameter undefined.
+export function getParameter(query: any, parameterName: string ): Promise<string> {
+    return new Promise(resolve => {
+      let parameterValue = query[parameterName];
+      if (!!parameterValue) {
+        resolve(parameterValue);
+      } else {
+        throw `${parameterName} is required`;
+      }
+    });
+  }
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+
+    if (!req.headers || !req.headers.authorization) {
+        return res.status(StatusCodes.UNAUTHORIZED).send({ message: 'No authorization headers.' });
+    }
+
+    const token_bearer = req.headers.authorization.split(' ');
+    if (token_bearer.length != 2) {
+        return res.status(StatusCodes.UNAUTHORIZED).send({ message: 'Malformed token.' });
+    }
+
+    const token = token_bearer[1];
+
+    return next();
+
+    // return jwt.verify(token, "hello", (err, decoded) => {
+    //   if (err) {
+    //     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ auth: false, message: 'Failed to authenticate.' });
+    //   }
+    //   return next();
+    // });
 }
